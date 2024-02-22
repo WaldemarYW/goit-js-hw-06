b9794890c533a46c8dc6d5c0120198f8bdfa3b2b
WaldemarYW <124668448+WaldemarYW@users.noreply.github.com>
2024-02-22 13:06:06 +0200

    vel 1
    not easy

---
+++ js/task-2.js
@@ -0,0 +1,24 @@
+class Storage {
+  #items;
+  constructor(items) {
+    this.#items = items;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    const indexDell = this.#items.indexOf(itemToRemove);
+    if (indexDell !== -1) {
+      this.#items.splice(indexDell, 1);
+    }
+  }
+}
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
